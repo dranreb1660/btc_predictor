e9aeeea946535fdd50c46c3e63b0ab8ed1bb6234
Bernard <Bernardopoku160@yahoo.com>
2022-01-31 11:46:34 -0500

    removed src for simplification

--- btc_predictor/data/lit_data_model.py
+++ btc_predictor/data/lit_data_model.py
@@ -8,11 +8,11 @@ from btc_predictor.data.base_dataset import BTCDataset
 
 
 class BTCPriceDataModule(pl.LightningDataModule):
-    def __init__(self, train_sequences, val_sequences, batch_sz=8):
+    def __init__(self, train_sequences, val_sequences, batch_size=8):
         super().__init__()
         self.train_sequences = train_sequences
         self.val_sequences = val_sequences
-        self.batch_size = batch_sz
+        self.batch_size = batch_size
 
     def setup(self, stage=None):
         self.train_dataset = BTCDataset(self.train_sequences)
@@ -26,12 +26,12 @@ class BTCPriceDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(
-            self.val_dataset, batch_sz=1,
+            self.val_dataset, batch_size=1,
             num_workers=1
         )
 
     def test_dataloader(self):
         return DataLoader(
-            self.val_dataset, batch_sz=1,
+            self.val_dataset, batch_size=1,
             num_workers=1
         )
